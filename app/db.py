3f57d6ab90167909b449298fca355b791a2b71f1
saraung <saraungbabu@gmail.com>
2025-12-28 21:58:23 +0530

    complete api

--- app/db.py
+++ app/db.py
@@ -14,12 +14,13 @@ class Base(DeclarativeBase):
     pass
 
 class User(SQLAlchemyBaseUserTableUUID,Base):
-    posts= relationship("Post",back_populates="user")
+    __tablename__ = "users"
+    posts= relationship("Post",back_populates="user",foreign_keys="Post.user_id")
 class Post(Base):
     __tablename__ = "posts"
 
     id=Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
-    user_id=Column(UUID(as_uuid=True),ForeignKey("user.id"),nullable=False)
+    user_id=Column(UUID(as_uuid=True),ForeignKey("users.id"),nullable=False)
     caption=Column(Text)
     url=Column(String,nullable=False)
     file_type=Column(String,nullable=False)
